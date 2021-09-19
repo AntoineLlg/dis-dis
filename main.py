e84b64af2eeaa97c230ff49e8db65786c5cd56e1
taucmar <antoinelelong.pro@gmail.com>
2021-09-19 20:23:06 +0200

    Add the main.py file

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+import re
+
+class MyClient(discord.Client) :
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        
+        
+    async def on_message(self,message):
+        if message.author == self.user :
+            return
+        else:
+            speak = re.compile("[dD][iyIY][a-zA-ZéèêàÉÈÀ]+")
+            it = speak.finditer(message.content)
+            for match in it:
+                mot = match.group()
+                await message.channel.send(mot[2:])
+                
+            shout = re.compile(r"[cC][hH]?[rR][iIyY][a-zA-ZéèêàÉÈÀ]+")
+            it = shout.finditer(message.content)
+            for match in it:
+                mot = match.group()
+                bord = 3
+                if mot[1] in ['h','H']:
+                    bord = 4
+                await message.channel.send(mot[bord:].upper())
+
+
+bot = MyClient()
+bot.run('insert token')
